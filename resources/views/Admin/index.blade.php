8ede9b8a1aba29461d63063b47c059bb92f16fa5
finter <finter@gmail.com>
2020-07-12 12:23:01 +0430

    debug css files and header box

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -23,26 +23,6 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box">
-                        <div class="page-title-right">
-                            <form class="form-inline">
-                                <div class="form-group">
-                                    <div class="input-group input-group-sm">
-                                        <input type="text" class="form-control border-0 shadow" id="dash-daterange">
-                                        <div class="input-group-append">
-                                            <span class="input-group-text bg-blue border-blue text-white">
-                                                <i class="mdi mdi-calendar-range"></i>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <a href="javascript: void(0);" class="btn btn-blue btn-sm ml-2">
-                                    <i class="mdi mdi-autorenew"></i>
-                                </a>
-                                <a href="javascript: void(0);" class="btn btn-blue btn-sm ml-1">
-                                    <i class="mdi mdi-filter-variant"></i>
-                                </a>
-                            </form>
-                        </div>
                         <h4 class="page-title">داشبورد</h4>
                     </div>
                 </div>
