64761bc392eeb2222b1a878791fa73e9c12654c2
finter <finter@gmail.com>
2020-07-26 10:36:49 +0430

    add intro panel page

--- routes/web.php
+++ routes/web.php
@@ -192,4 +192,7 @@ Route::post('/gamenet/rate', 'HomeController@rate')->name('gamenet.rate');
 Route::get('/user/panel' , function(){
     return view('panel');
 });
+Route::get('/intro/panel' , function(){
+    return view('intropanel');
+})->name('intro.panel');
 /*End Test Routes*/
