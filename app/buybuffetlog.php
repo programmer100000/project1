04cd41387c3a323f497e459b6c14752b8e628272
sam sepiol <samsepiol@localhost.localdomain>
2020-07-29 14:08:02 +0430

    resolve conflicts

--- app/buybuffetlog.php
+++ app/buybuffetlog.php
@@ -7,9 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class buybuffetlog extends Model
 {
     protected $table = 'buffets_buy_log';
-<<<<<<< HEAD
     protected $primarykey = 'buffet_buy_log_id';
-=======
-    protected $primaryKey = 'buffet_buy_log_id';
->>>>>>> 919bfad9f501c9b13ca2bfd79d9413d4f5a00040
 }
